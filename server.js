58dae2e7d322282c9601883e9840bff618051794
Zeca Costa <jccostaso@gmail.com>
2021-06-04 19:36:15 -0300

    iniciando o projeto

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  }});
+
+app.use(express.static(__dirname + '/public'));
+
+require('./sockets/server-io')(io);
+
+const PORT = 3000;
+
+http.listen(PORT, () => {
+  console.log('App listening on PORT', PORT)
+});
---
+++ sockets/server-io.js
@@ -0,0 +1,16 @@
+module.exports = (io) => {
+
+  io.on('connection', (socket) => {
+    
+    socket.on('sendUserToServer', (user) => {
+      console.log(`novo usuário conectado! ${user}`);
+
+      socket.emit('confirmConnection', user);
+      socket.broadcast.emit('newUserConnect', user);
+    });
+ 
+    socket.on('sendMessageToServer', (message) => {
+      io.emit('sendMessageToClients', message);
+    });
+  });
+};
